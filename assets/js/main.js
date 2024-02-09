70333645cff5b8aab75574a9849aa7bd242cc9d7
Williams Hernández <hernandw@gmail.com>
2024-02-09 16:59:36 -0300

    :sparkles: 3er requerimiento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-
-
-
 //iterar el array de objetos con un ciclo for
 /* for (let i = 0; i < dental.length; i++){
     console.log(dental[i].price)
@@ -16,8 +13,6 @@
 for (let diente of dental) {
     console.log(diente.name)
 } */
-
-
 
 const dental = [
   {
@@ -64,13 +59,25 @@ const dental = [
   },
 ];
 
-
 //segundo requerimiento mostrar la primera y ultima consulta npmbre y prevision"
-
 
 document.write(`<p>Primera atencion: ${dental[0].paciente} -
 ${dental[0].prevision} | Última atención: ${
   dental[dental.length - 1].paciente
 } - ${dental[dental.length - 1].prevision}.</p>`);
 
+let tabla = "";
+for (let i = 0; i < dental.length; i++) {
+  tabla += `
+  <tr>
+  <td>${dental[i].hora}</td>
+  <td>${dental[i].especialista}</td>
+  <td>${dental[i].paciente}</td>
+  <td>${dental[i].rut}</td>
+  <td>${dental[i].prevision}</td>
+  </tr>`;
+}
 
+console.log(tabla)
+
+document.getElementById('cuerpo').innerHTML = tabla
